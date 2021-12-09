0ce3a34c29907fe0bcb2b093d0bd7257e0f78ae9
stovet <steven.topalian@gmail.com>
2021-12-09 16:00:49 -0500

    links showing up

--- script.js
+++ script.js
@@ -5,19 +5,21 @@ const awwData = fetch("https://www.reddit.com/r/aww/.json")
 
 awwData.then(data => {
     for(let i =0; i < data.data.children.length; i++){
-        //console.log(data.data.children[i].data.title);
         let newDiv = document.createElement('div');
         newDiv.append(data.data.children[i].data.title);
+
         let newImage = document.createElement('img');
+        newImage.classList.add('image');
         newImage.setAttribute('src', data.data.children[i].data.thumbnail)
         newDiv.append(newImage);
+
         let newLink = document.createElement('a');
-        
-        
-        //newDiv.append(data.data.children[i].data.thumbnail);
+        newLink.innerHTML = "click me";
+        newLink.href = "https://reddit.com" + data.data.children[i].data.permalink;
+
+        newDiv.append(newLink);   
         div.append(newDiv);
-        //console.log(data.data.children[i].data.thumbnail)
-        //body.append(data.data.children[i].data.thumbnail);
+
     }
     
 })
